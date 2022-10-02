fa38063baf9e0a950fddf52fd1015904073a92cc
randytravis <104604002+randytravis@users.noreply.github.com>
2022-10-02 07:35:03 +0000

    Added basic html parser setup

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print('hello world')
+import requests
+from bs4 import BeautifulSoup
+
+TRENDING_URL = 'https://trends.google.com/trends/trendingsearches/realtime?geo=US&category=all'
+
+response = requests.get(TRENDING_URL)
+
+print('Status Code', response.status_code)
+
+with open('trending.html', 'w') as f:
+  f.write(response.text)
+
+doc = BeautifulSoup(response.text, 'html.parser')
+
+print('Page title:', doc.title)
+
+#find all trends list divs
+trends_divs = doc.find_all('div', class_='feed-item-header')
+
+print(f'Found {len(trends_divs)} results')
