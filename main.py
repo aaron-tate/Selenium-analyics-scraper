aca0665af52cd4fc9a12be8321c6135a501ed70b
randytravis <104604002+randytravis@users.noreply.github.com>
2022-10-08 15:05:39 +0000

    updating xpath for url

--- main.py
+++ main.py
@@ -4,19 +4,21 @@ from selenium.webdriver.common.by import By
 
 TRENDING_URL = 'https://trends.google.com/trends/trendingsearches/realtime?geo=US&category=all'
 
+
 def get_driver():
   chrome_options = Options()
   chrome_options.add_argument('--no-sandbox')
   chrome_options.add_argument('--headless')
   chrome_options.add_argument('--disable-dev-shm-usage')
   driver = webdriver.Chrome(options=chrome_options)
-  return driver 
+  return driver
+
 
 def get_items(driver):
-    driver.get(TRENDING_URL)
-    T_ITEMS_DIV_TAG = 'md-list'
-    items = driver.find_elements(By.TAG_NAME, T_ITEMS_DIV_TAG)
-    return items
+  driver.get(TRENDING_URL)
+  T_ITEMS_DIV_TAG = 'md-list'
+  items = driver.find_elements(By.TAG_NAME, T_ITEMS_DIV_TAG)
+  return items
 
 if __name__ == "__main__":
   print('Creating Driver')
@@ -24,22 +26,26 @@ if __name__ == "__main__":
 
   print('Fetching the Trending topics')
   items = get_items(driver)
-  
+
   print(f'Found {len(items)} items')
 
   print('parsing the first topic')
   #info being parsed: rank in top ten, title, news outlet, short description, and thumbnail
 
   item = items[1]
-  title_CLASS= item.find_element(By.TAG_NAME, 'a')
+  title_CLASS = item.find_element(By.TAG_NAME, 'a')
   title = title_CLASS.text
 
   description_tag = item.find_element(By.CLASS_NAME, 'summary-text')
-  description = description_tag.text 
+  description = description_tag.text
+
+  URL_tag = item.find_element(By.XPATH, "//md-list[1]/feed-item/ng-include/div/div/div[1]/div[2]/div[2]/div[1]/div[1]/a")
+  url = URL_tag.get_attribute('href')
 
   source_tag = item.find_element(By.CLASS_NAME, 'source-and-time')
   source = source_tag.text
-  
+
   print('Title:', title)
   print('Short Description:', description)
   print('', source)
+  print('', url)
